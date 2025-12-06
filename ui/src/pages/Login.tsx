f24c38a28a43c8657f69957c91278bbcd7e1ee07
Topvennie <vincent@vallaeys.com>
2025-12-06 18:20:21 +0100

    chore(login): make text more visible

--- ui/src/pages/Login.tsx
+++ ui/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from "@/lib/hooks/useAuth";
-import { Button, Center, Paper, Stack, Text, Title } from "@mantine/core";
+import { Button, Center, Paper, Stack, Title } from "@mantine/core";
 import { FaSpotify } from "react-icons/fa6";
 
 export const Login = () => {
@@ -7,10 +7,10 @@ export const Login = () => {
 
   return (
     <Center h="100vh">
-      <Paper shadow="sm" p="xl" className="w-96">
+      <Paper radius="md" shadow="sm" p="xl" className="w-96">
         <Stack align="center">
-          <Title>Login</Title>
-          <Text c="gray">Sortifyr</Text>
+          <Title order={1}>Login</Title>
+          <p className="text-muted">Sortifyr</p>
           <Button onClick={login} size="lg" className="my-12">
             <FaSpotify size={"1.7rem"} className="mr-2" />
             Spotify
